29fdc771802e81e10d2aa33e64e900a945804b39
Greed <122706763+Superior-Greed@users.noreply.github.com>
2023-01-18 16:32:35 -0800

    Update db.py

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQL_URL = "mysql://root:Dragon12@@localhost:3306/practica"
+SQL_URL = "mysql://root:s@localhost:3306/practica"
 engine = create_engine(SQL_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
