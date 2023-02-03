672e2292b6aa9c1c840ee5dbad75601aa644b413
Superior-Greed <aporou205@gmail.com>
2023-02-02 19:46:39 -0800

    agregando datos finales

--- schema/user.py
+++ schema/user.py
@@ -1,12 +1,14 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel #,EmailStr
 
+#email cambiar str por email
+#session_init poner optional[datetime]
 class UserSchema(BaseModel):
     id: int
     user_name:str
     name:str
     last_name: str
-    email: str
+    email: str#EmailStr
     password:str
     image:str | None
     #session_init:datetime | None
@@ -15,18 +17,6 @@ class RoleSchema(BaseModel):
     id:int
     name:str
     description:str | None
-
-class UsersRolesSchema(BaseModel):
-    id_user:int
-    users: list[UserSchema] = [] 
-    id_role:int
-    roles: list[RoleSchema] = [] 
-    
-class UserRolesSchema(BaseModel):
-    id_user:int
-    user: UserSchema
-    id_role:int
-    roles:list [RoleSchema] = [] 
 
 class UserRoleSchema(BaseModel):
     user_id:int
